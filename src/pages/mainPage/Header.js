9136d5cf499fa6908d401099e269313953caf015
Onur Özdamar <63419558+onurozdamar@users.noreply.github.com>
2021-05-15 19:11:33 +0300

    navigation button

--- src/pages/mainPage/Header.js
+++ src/pages/mainPage/Header.js
@@ -1,34 +1,53 @@
 import "./Header.css";
 import logo from "../../img/siyah_.png";
+import React, { useState } from "react";
 
 function Header() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   return (
-    <header>
+    <header className={isNavOpen ? "nav-open" : ""}>
       <div className="logo">
         <img src={logo} alt="logo" />
       </div>
-      <button className="nav-toggle" aria-label="toggle navigation">
+      <button className="nav-toggle" onClick={() => setIsNavOpen(!isNavOpen)}>
         <span className="hamburger"></span>
       </button>
       <nav className="nav">
         <ul className="nav__list">
           <li className="nav__item">
-            <a href="#home" className="nav__link">
+            <a
+              href="#home"
+              className="nav__link"
+              onClick={() => setIsNavOpen(!isNavOpen)}
+            >
               Home
             </a>
           </li>
           <li className="nav__item">
-            <a href="#services" className="nav__link">
+            <a
+              href="#services"
+              className="nav__link"
+              onClick={() => setIsNavOpen(!isNavOpen)}
+            >
               My Servics
             </a>
           </li>
           <li className="nav__item">
-            <a href="#about" className="nav__link">
+            <a
+              href="#about"
+              className="nav__link"
+              onClick={() => setIsNavOpen(!isNavOpen)}
+            >
               About me
             </a>
           </li>
           <li className="nav__item">
-            <a href="#work" className="nav__link">
+            <a
+              href="#work"
+              className="nav__link"
+              onClick={() => setIsNavOpen(!isNavOpen)}
+            >
               My Work
             </a>
           </li>
