b3389558a5f0a11087fdb9c5f27cf1b0aa500e1c
Onur Özdamar <63419558+onurozdamar@users.noreply.github.com>
2021-10-24 14:05:28 +0300

    set header with mui

--- src/pages/mainPage/Header.js
+++ src/pages/mainPage/Header.js
@@ -1,60 +1,139 @@
-import "./Header.css";
-import logo from "../../img/siyah_.png";
-import React, { useState } from "react";
+import { AutoStories, EmojiObjects, Home, Person } from "@mui/icons-material";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Avatar, Link } from "@mui/material";
+import MuiAppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { styled, useTheme } from "@mui/material/styles";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import logo from "../../img/logo.png";
 
-function Header() {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+const drawerWidth = 240;
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  transition: theme.transitions.create(["margin", "width"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["margin", "width"], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginRight: drawerWidth,
+  }),
+}));
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: "flex-start",
+}));
+
+export default function PersistentDrawerRight() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <header className={isNavOpen ? "nav-open" : ""}>
-      <div className="logo">
-        <img src={logo} alt="logo" />
-      </div>
-      <button className="nav-toggle" onClick={() => setIsNavOpen(!isNavOpen)}>
-        <span className="hamburger"></span>
-      </button>
-      <nav className="nav">
-        <ul className="nav__list">
-          <li className="nav__item">
-            <a
-              href="#home"
-              className="nav__link"
-              onClick={() => setIsNavOpen(!isNavOpen)}
-            >
-              Anasayfa
-            </a>
-          </li>
-          <li className="nav__item">
-            <a
-              href="#services"
-              className="nav__link"
-              onClick={() => setIsNavOpen(!isNavOpen)}
-            >
-              Uğraşlarım
-            </a>
-          </li>
-          <li className="nav__item">
-            <a
-              href="#about"
-              className="nav__link"
-              onClick={() => setIsNavOpen(!isNavOpen)}
-            >
-              Hakkımda
-            </a>
-          </li>
-          <li className="nav__item">
-            <a
-              href="#work"
-              className="nav__link"
-              onClick={() => setIsNavOpen(!isNavOpen)}
-            >
-              Projelerim
-            </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <Avatar alt="lgoo" src={logo} variant="rounded" />
+          <Typography
+            variant="h6"
+            noWrap
+            sx={{ flexGrow: 1, marginLeft: 1 }}
+            component="div"
+          >
+            Onur Özdamar
+          </Typography>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="end"
+            onClick={handleDrawerOpen}
+            sx={{ ...(open && { display: "none" }) }}
+          >
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+          },
+        }}
+        variant="persistent"
+        anchor="right"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          <ListItem button component={Link} href="#home">
+            <ListItemIcon>
+              <Home />
+            </ListItemIcon>
+            <ListItemText primary={"Anasayfa"} />
+          </ListItem>
+          <ListItem button component={Link} href="#services">
+            <ListItemIcon>
+              <AutoStories />
+            </ListItemIcon>
+            <ListItemText primary={"Uğraşlarım"} />
+          </ListItem>
+          <ListItem button component={Link} href="#about">
+            <ListItemIcon>
+              <Person />
+            </ListItemIcon>
+            <ListItemText primary={"Hakkımda"} />
+          </ListItem>
+          <ListItem button component={Link} href="#work">
+            <ListItemIcon>
+              <EmojiObjects />
+            </ListItemIcon>
+            <ListItemText primary={"Projelerim"} />
+          </ListItem>
+        </List>
+      </Drawer>
+    </Box>
   );
 }
-
-export default Header;
