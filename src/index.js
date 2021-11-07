32120ec01052a89f77610a27babd46f71716493b
Onur Özdamar <63419558+onurozdamar@users.noreply.github.com>
2021-11-07 19:50:45 +0300

    add  routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import { render } from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
+import Abalone from "./pages/projects/Abalone";
+import Calis from "./pages/projects/Calis";
+import Ingilizce from "./pages/projects/Ingilizce";
+import Shoplist from "./pages/projects/Shoplist";
+import Sudoku from "./pages/projects/Sudoku";
+import Xox from "./pages/projects/Xox";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const rootElement = document.getElementById("root");
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/abalone" element={<Abalone />} />
+      <Route path="/calis" element={<Calis />} />
+      <Route path="/inglizce" element={<Ingilizce />} />
+      <Route path="/shoplist" element={<Shoplist />} />
+      <Route path="/sudoku" element={<Sudoku />} />
+      <Route path="/xox" element={<Xox />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
