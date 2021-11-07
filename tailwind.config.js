18ba5edf7864a33001be163e07fbd93960a14fa2
Onur Özdamar <63419558+onurozdamar@users.noreply.github.com>
2021-11-07 18:36:32 +0300

    add grid template columns

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,18 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        projects: "repeat(auto-fit, minmax(300px, 1fr))",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      transform: ["hover", "focus"],
+      scale: ["hover", "focus"],
+      zIndex: ["hover", "active"],
+    },
   },
   plugins: [],
 };
